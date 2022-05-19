51a6eb38f4723fad82c56058e5d43d3de35522da
twiskkk <105844209+twiskkk@users.noreply.github.com>
2022-05-19 09:03:20 +0200

    Update formulaires.php

--- formulaires.php
+++ formulaires.php
@@ -117,37 +117,35 @@ function afficheFormulaireChoixUtilisateur(){
 	}// fin afficheFormulaireChoixUtilisateur
 
 //*******************************************************************************************
-function ajoutLycee($noL,$nom,$adresse,$codepostal,$telephone){		/* on récupère directement le code de la ville qui a été transmis dans l'attribut value de la balise <option> du formulaire
-    Il n'est donc pas nécessaire de rechercher le code INSEE de la ville*/
-    $retour=0;
-    $madb = new PDO('sqlite:bdd/bdd.sqlite'); 	
-    // filtrer les paramètres
-    $noL = $madb->quote($noL);		
-    $nom = $madb->quote($nom);
-    $adresse= $madb->quote($adresse);
-    $codepostal = $madb->quote($codepostal);
-    $telephone = $madb->quote($telephone);
-
-
-    //Recuperer la ville grace au code postal
-    $ville = "SELECT ville FROM lycee where codepostal == $codepostal;";
-    $query= $madb->query($ville);
-    if($query)
-    {
-        $res = $query->fetch(PDO::FETCH_ASSOC);
-        $ville = $res['ville'];
-    }
-    $ville = $madb->quote($ville);
-    var_dump($ville,$noL);
-    // requête
-    $requete ="INSERT INTO lycee VALUES ($noL,$nom,$adresse,$codepostal,$ville,$telephone);";
-    $resultat =$madb->exec($requete);
-    if ($resultat == false ) 
-        $retour = 0;
-    else 
-        $retour = $resultat;
-    return $retour;
-
-}
+function afficheFormulaireAjoutLycee()    {
+    	// connexion BDD et récupération des villes
+		$madb = new PDO('sqlite:bdd/bdd.sqlite'); 
+        
+		$requete = "SELECT codepostal,ville FROM lycee;";
+		$resultat = $madb->query($requete);//var_dump($resultat);echo "<br/>";  
+		if($resultat){
+			$lycees = $resultat->fetchAll(PDO::FETCH_ASSOC);			
+		}
+	?>
+	<form  action="<?php echo $_SERVER['PHP_SELF']; ?>" method="post" >
+		<fieldset> 
+            <label for="id_tel">noL : </label> <input type="text" name="noL" id="id_noL" placeholder="LYC_XX" required size="5" /><br />
+			<label for="id_nom">Nom : </label> <input type="text" name="nom" id="id_nom" placeholder="Nom" required size="20" /><br />
+			<label for="id_adresse">Adresse : </label> <input type="text" name="adr" required id="id_adr" size="10"  placeholder="adresse" /><br />
+			<label for="id_tel">Téléphone : </label> <input type="tel" name="tel" id="id_tel" placeholder="06 XX XX XX XX" required size="10" /><br />
+			<label for="id_ville">Ville : </label> 
+			<select id="id_ville" name="ville_ly" size="1">
+				<?php // on se sert de value directement pour l'insertion					
+					foreach($lycees as $lycee){					
+						echo '<option value="'.$lycee['codepostal'].'">'.$lycee["codepostal"].' '.$lycee["ville"].'</option>';
+					}					
+				?>
+			</select>
+			<input type="submit" value="Insérer"/>
+		</fieldset>
+	</form>
+	<?php
+		echo "<br/>";
+	}// fin afficheFormulaireAjoutLycee
 
 ?>
