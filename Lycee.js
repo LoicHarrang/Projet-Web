1e807c1b15472870df6425be24a2ababbaa7e12f
twiskkk <105844209+twiskkk@users.noreply.github.com>
2022-05-24 19:06:52 +0200

    Modification seance

--- Lycee.js
+++ Lycee.js
@@ -1,5 +1,16 @@
 //*******************************************************************************************
-function mdpValide(mdp){
-    var reg = /^[a-z0-9._-!*?]+[a-z0-9.-!*?]{8,}[.][a-z]{2,3}$/;
-    return (reg.test(mdp)); // retourne Vrai ou Faux
+function mdpValide(){
+    var mdp = document.getElementById(id_pass).value;
+    console.log(mdp);
+    var res = false
+    var pass = /^(?=.{8,}$)(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*\W).*$/;
+    res = pass.test(mdp);
+    if (res == 1) {
+    console.log(res);
+    }
+    else {
+        console.log('erreur');
+    }
+    return false; // retourne Vrai ou Faux
+
 }
