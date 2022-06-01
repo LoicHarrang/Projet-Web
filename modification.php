5121a333505c1c67fcf90b58b9abd1f243204484
twiskkk <105844209+twiskkk@users.noreply.github.com>
2022-06-01 11:04:19 +0200

    modification ajax v2

--- modification.php
+++ modification.php
@@ -10,6 +10,9 @@ include 'contenue.php';
     <meta charset="utf-8">
     <title>Lycée - Modification</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <script src="ajax/Ajax_Modification.js" type="text/javascript"></script>
+
+
 </head>
 <body class="text-center">
 <?php
@@ -23,17 +26,23 @@ else if($_SESSION['statut']!='administrateur')
 }
 else
 {
+
     afficherMenuAdmin();
     contenue_modification();
     afficheFormulaireChoixUtilisateur();
+    var_dump($res,$_SESSION);
+    /*
     if(isset($_POST['noL']))
     {
         afficheFormulaireModification($_POST['noL']);
     }
+    */
     if(isset($_POST['nom']) && isset($_POST['adr']) && isset($_POST['tel']))
     {
         modifierLycee($_SESSION['modif'],$_POST['nom'],$_POST['adr'],$_POST['tel']);
+
     }
+    echo "<p id='test'></p>";
 }
 ?>
 
